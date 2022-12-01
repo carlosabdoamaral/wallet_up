e185e46884086dea6271a4c29b6cef23e9336060
Carlos Amaral <v-carlos.amaral@avenuesec.io>
2022-11-30 23:35:04 -0300

    feat/removing mobile

--- walletup/lib/common/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color red = Color.fromRGBO(237, 76, 92, 1);
-const Color green = Color.fromRGBO(149, 215, 139, 1);
-const Color grey = Color.fromRGBO(26, 25, 25, 1);
-const Color darkGrey = Color.fromRGBO(30, 30, 30, 1);
--- walletup/lib/common/variables.dart
+++
@@ -1,1 +0,0 @@
-double balanceValue = 0;
